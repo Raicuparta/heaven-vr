1400f8dc06d334b756fe86db4e6f92d87ceed5e6
Raicuparta <raicuparta@gmail.com>
2022-08-02 16:02:51 +0200

    Comment

--- HeavenVr/Patches.cs
+++ HeavenVr/Patches.cs
@@ -197,6 +197,13 @@ public static class Patches
         __instance.components.RemoveAll(component => component is IPostProcessComponent);
     }
     
+    /*
+     * Couldn't figure out how to add a new rendering pass, so I'm patching an existing rendering path instead.
+     * The missing link to adding new rendering passes seems to be ForwardRendererData.m_RendererFeatureMap.
+     * m_RendererFeatureMap is set in the Unity Editor and the value seems to be based on a file, so I couldn't figure
+     * out a way around it.
+     * The class SDKUniversalRenderFeature is only used for storing the passes property, which is accessed in LIV code.
+     */
     [HarmonyPostfix]
     [HarmonyPatch(typeof(BeautifyRendererFeature), nameof(BeautifyRendererFeature.AddRenderPasses))]
     private static void AddLivRenderPasses(BeautifyRendererFeature __instance, ScriptableRenderer renderer, ref RenderingData renderingData)
