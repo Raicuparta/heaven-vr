f2ffe24472b0e74c8a9c34011fe81ff87cb094cf
Raicuparta <raicuparta@gmail.com>
2022-08-04 03:27:34 +0200

    render texture ui sorta working

--- HeavenVr/VrAssetLoader.cs
+++ HeavenVr/VrAssetLoader.cs
@@ -9,11 +9,13 @@ public static class VrAssetLoader
 {
     private const string assetsDir = "/BepInEx/plugins/HeavenVr/Assets/";
     public static GameObject RunAnimationPrefab;
+    public static GameObject VrUiQuadPrefab;
     
     public static void Init()
     {
         SDKShaders.LoadFromAssetBundle(LoadBundle("liv-shaders"));
         RunAnimationPrefab = LoadBundle("animation").LoadAsset<GameObject>("RunAnimation");
+        VrUiQuadPrefab = LoadBundle("ui").LoadAsset<GameObject>("VrUiQuad");
     }
     
     private static AssetBundle LoadBundle(string assetName)
