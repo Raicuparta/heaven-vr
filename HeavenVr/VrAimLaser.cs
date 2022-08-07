66f392fb32e6b00001fcf734ed2272c6cba9c1fa
Raicuparta <raicuparta@gmail.com>
2022-08-07 15:09:05 +0200

    Add gun to game

--- HeavenVr/VrAimLaser.cs
+++ HeavenVr/VrAimLaser.cs
@@ -10,24 +10,10 @@ public class VrAimLaser: MonoBehaviour
 
     public static VrAimLaser Create(Transform parent)
     {
-        var instance = new GameObject("VrAimLaser").AddComponent<VrAimLaser>();
+        var instance = parent.Find("Gun").GetOrAddComponent<VrAimLaser>();
         instance.transform.SetParent(parent, false);
+        LayerHelper.SetLayerRecursive(instance.gameObject, GameLayer.VrUi);
 
         return instance;
-    }
-    
-    private void Awake()
-    {
-        line = gameObject.AddComponent<LineRenderer>();
-        transform.SetParent(transform, false);
-        transform.localEulerAngles = Vector3.right * 60f;
-        line.useWorldSpace = false;
-        line.startWidth = 0.01f;
-        line.endWidth = 0f;
-        line.SetPositions(new []{ Vector3.zero, Vector3.forward * rayDistance });
-        line.material.shader = Shader.Find(lineShaderName);
-        // Using Ignore Raycast layer because it's visible in map camera and player camera.
-        // Might be better to use a custom layer, but seems like they're all being used.
-        gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
     }
 }
