5ab4add956c11af102aa8dc8342fa95303f71251
Raicuparta <raicuparta@gmail.com>
2022-08-21 13:51:52 +0200

    Cleanup

--- HeavenVr/src/Helpers/TypeExtensions.cs
+++ HeavenVr/src/Helpers/TypeExtensions.cs
@@ -13,18 +13,10 @@ public static class TypeExtensions
 		type.BaseType?.GetMethod(name, Flags) ??
 		type.BaseType?.BaseType?.GetMethod(name, Flags);
 
-	public static MemberInfo GetAnyMember(this Type type, string name) =>
+	private static MemberInfo GetAnyMember(this Type type, string name) =>
 		type.GetMember(name, Flags).FirstOrDefault() ??
 		type.BaseType?.GetMember(name, Flags).FirstOrDefault() ??
 		type.BaseType?.BaseType?.GetMember(name, Flags).FirstOrDefault();
-
-	public static T GetValue<T>(this object obj, string name) =>
-		obj.GetType().GetAnyMember(name) switch
-		{
-			FieldInfo field => (T) field.GetValue(obj),
-			PropertyInfo property => (T) property.GetValue(obj, null),
-			_ => default
-		};
 
 	public static void SetValue(this object obj, string name, object value)
 	{
@@ -38,13 +30,4 @@ public static class TypeExtensions
 				break;
 		}
 	}
-
-	public static void Invoke(this object obj, string name, params object[] parameters) => 
-		Invoke<object>(obj, name, parameters);
-
-	public static T Invoke<T>(this object obj, string name, params object[] parameters) => 
-		(T)obj.GetType().GetAnyMethod(name)?.Invoke(obj, parameters);
-		
-	public static T Invoke<T>(this Type type, string name, params object[] parameters) =>
-		(T)type.GetAnyMethod(name).Invoke(null, parameters);
 }
