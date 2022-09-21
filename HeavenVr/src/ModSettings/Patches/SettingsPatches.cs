a18d57f56357bd573497fb75230dd14fb9b1403b
Raicuparta <raicuparta@gmail.com>
2022-09-21 21:02:12 +0200

    add aiming offset to in-game settings

--- HeavenVr/src/ModSettings/Patches/SettingsPatches.cs
+++ HeavenVr/src/ModSettings/Patches/SettingsPatches.cs
@@ -1,4 +1,7 @@
-﻿using HarmonyLib;
+﻿using System.Linq;
+using HarmonyLib;
+using I2.Loc;
+using UnityEngine;
 
 namespace HeavenVr.ModSettings.Patches;
 
@@ -32,4 +35,51 @@ public static class SettingsPatches
         // TODO: submit vr scores to a separate leaderboard.
         GameDataManager.powerPrefs.dontUploadToLeaderboard = true;
     }
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(OptionsMenuTabManager), nameof(OptionsMenuTabManager.Start))]
+    private static void CreateMenuEntries(OptionsMenuTabManager __instance)
+    {
+        var panels = __instance._panels.Select(panel => panel.panel).ToArray();
+
+        var generalPanel = panels.First(panel => panel.name.StartsWith("General"));
+        var controlsPanel = panels.First(panel => panel.name.StartsWith("Controls"));
+        var audioPanel = panels.First(panel => panel.name.StartsWith("Audio"));
+        var videoPanel = panels.First(panel => panel.name.StartsWith("Video"));
+
+        var aimingAngleOffsetSlider = Object.Instantiate(controlsPanel._sliderPrefab, controlsPanel._settingsColumn.transform);
+
+        var aimingAngleOffsetOptionEntry = new OptionsMenuPanelInformation.OptionEntry()
+        {
+            SettingType = OptionMenuSetting.AimAssist,
+            SliderMaximum = VrSettings.MaxAngleOffset,
+            SliderMinimum = -VrSettings.MaxAngleOffset,
+            StepSize = 1,
+        };
+
+        var settingText = VrSettings.AimingAngleOffset.Description.Description.Split('|');
+        var settingTitle = settingText[0];
+        var settingDescription = settingText[1];
+
+        aimingAngleOffsetSlider.Initialise(VrSettings.AimingAngleOffset.Value,
+            settingTitle,
+            aimingAngleOffsetOptionEntry,
+            () => controlsPanel._TipWindow.SetWindowTip(settingTitle, settingDescription),
+            () => controlsPanel._TipWindow.ResetWindow());
+    }
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(LocalizationManager), nameof(LocalizationManager.TryGetTranslation))]
+    private static void PassThroughMissingTranslations(ref bool __result, string Term, ref string Translation)
+    {
+        // Usually missing translations cause errors to be thrown.
+        // I'm making it so missing translations just return the translation key text.
+        // This way I can just pass the replacement text as the key.
+        // I should probably have my own dictionary instead, but this is easier.
+        if (__result) return;
+
+        Translation = Term;
+        __result = true;
+    }
+    
 }
